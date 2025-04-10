eab98e18a8834399761925a6a4b6925f05c8fb7a
Yasmin-Lotfy <yasmine.lotfy11@gmail.com>
2025-04-10 18:45:15 +0200

    Update useFetchHome.ts

--- src/hooks/useFetchHome.ts
+++ src/hooks/useFetchHome.ts
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 
 
 // Fetch function
-const fetchData = async <T>(endpoint: string): Promise<HomeProductApiResponse<T>> => {
+const fetchData = async <T = HomeProductApiResponse>(endpoint: string): Promise<T> => {
   const res = await fetch(`https://flower.elevateegy.com/api/v1/${endpoint}`); // Replace with your API base URL
   if (!res.ok) throw new Error(`Failed to fetch data from ${endpoint}`);
   return res.json();
